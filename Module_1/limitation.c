ca051c556a4b405ee50ebf73b00043e01293b2f9
hidertanveer <hidertanveer9@gmail.com>
2023-04-03 16:05:17 +0600

    Module 1

---
+++ Module_1/limitation.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    double d = 12.454215454123457;
+    printf("%0.15lf",d);
+    return 0;
+}
---
+++ Module_1/variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a=7;
+    float b = 12.4567;
+    double c = 12.12345782;
+    char d ='abc';
+    char s[] ="Hey This is a String Variable";
+    printf("Integer Variable = %d\nFloat variable = %0.3f\nDouble variable = %0.5lf\nCharacter Vaiable = %c\nString Variable = %s",a,b,c,d,s);
+
+   return 0;
+}
